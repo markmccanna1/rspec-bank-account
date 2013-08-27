ea5d0caa404f875bb4b0c96b242e7fc274bfdc35
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 10:35:23 -0500

    fixed indentation and replaced before with let

--- account_spec.rb
+++ account_spec.rb
@@ -4,14 +4,10 @@ require_relative "account"
 
 describe Account do
 
-  before(:each) do
-    @account = Account.new "1234567890", 15
-  end
+  let(:account) {Account.new("1234567890", 15)}
 
   describe "#initialize" do
     it "takes 2 arguments" do
-      #should this be account?
-
       Account.should respond_to(:new).with(2).arguments
     end
 
@@ -22,49 +18,49 @@ describe Account do
     end
 
     it "should be an instance of Account" do
-      @account.should be_an_instance_of Account
+      account.should be_an_instance_of Account
     end
   end
 
   describe "#transactions" do
     it "returns the correct transactions number" do
-      @account.transactions.should eql [15]
+      account.transactions.should eql [15]
     end
 
     it "returns an array of transactions" do
-      @account.transactions.should be_an_instance_of Array
+      account.transactions.should be_an_instance_of Array
     end
   end
 
   describe "#balance" do
     it "returns the correct balance" do
-      @account.balance.should eql 15
+      account.balance.should eql 15
     end
   end
 
   describe "#account_number" do
     it "returns the correct account_number" do
-     @account.acct_number.should eql "******7890"
+      account.acct_number.should eql "******7890"
     end
   end
 
   describe "#deposit!" do
     it "should raise an exception if you deposit less than 0" do
-      expect {@account.deposit!(-5)}.to raise_error(NegativeDepositError)
+      expect {account.deposit!(-5)}.to raise_error(NegativeDepositError)
     end
 
     it "should return the correct balance" do
-      @account.deposit!(5).should eql 20
+      account.deposit!(5).should eql 20
     end
   end
 
   describe "#withdraw!" do
     it "should raise an exception if you withdraw more than your balance" do
-      expect {@account.withdraw!(100)}.to raise_error(OverdraftError)
+      expect {account.withdraw!(100)}.to raise_error(OverdraftError)
     end
 
     it "should return the correct balance" do
-      @account.withdraw!(5).should eql 10
+      account.withdraw!(5).should eql 10
     end
   end
 end
